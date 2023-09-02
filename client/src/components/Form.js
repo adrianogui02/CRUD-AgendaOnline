3c8d46385f4473a2de38c6e4aa2fea76b5ee0e4b
Adriano Nobre <adrianonobrevg02@gmail.com>
2023-09-02 13:25:30 -0400

    First commit

---
+++ client/src/components/Form.js
@@ -0,0 +1,133 @@
+import axios from "axios";
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { toast } from "react-toastify";
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 30px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 20px;
+  width:120%
+  
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+`;
+
+const Input = styled.input`
+  width: 180px;
+  padding: 0 10px;
+  border: 1px solid #EDEDED;
+  border-radius: 5px;
+  height: 40px;
+  background-color: #EDEDED;
+  
+`;
+
+const Label = styled.label`
+  justify-content:center
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #7B38FF;
+  color: white;
+  height: 42px;
+  width:100px
+`;
+
+const Form = ({ getUsers, onEdit, setOnEdit }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (onEdit) {
+      const user = ref.current;
+
+      user.nome.value = onEdit.nome;
+      user.email.value = onEdit.email;
+      user.fone.value = onEdit.fone;
+      user.data_nascimento.value = onEdit.data_nascimento;
+    }
+  }, [onEdit]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = ref.current;
+
+    if (
+      !user.nome.value ||
+      !user.email.value ||
+      !user.fone.value ||
+      !user.data_nascimento.value
+    ) {
+      return toast.warn("Preencha todos os campos!");
+    }
+
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          nome: user.nome.value,
+          email: user.email.value,
+          fone: user.fone.value,
+          data_nascimento: user.data_nascimento.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800", {
+          nome: user.nome.value,
+          email: user.email.value,
+          fone: user.fone.value,
+          data_nascimento: user.data_nascimento.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    user.nome.value = "";
+    user.email.value = "";
+    user.fone.value = "";
+    user.data_nascimento.value = "";
+
+    setOnEdit(null);
+    getUsers();
+  };
+
+  return (
+    <FormContainer ref={ref} onSubmit={handleSubmit}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email" type="email" />
+      </InputArea>
+      <InputArea>
+        <Label>Telefone</Label>
+        <Input name="fone" />
+      </InputArea>
+      <InputArea>
+        <Label>Data de Nascimento</Label>
+        <Input name="data_nascimento" type="date" />
+      </InputArea>
+
+      <Button type="submit">Adicionar</Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
